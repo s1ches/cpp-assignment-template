f4f27e2496367eadfbee86df6e49bbe85ae4fa40
1chess <1chessMic@gmail.com>
2023-09-16 20:17:02 +0300

    hw01

---
+++ hw01/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ivan/Programming/C++/cpp-assignment-template/hw01/src
+# Build directory: /home/ivan/Programming/C++/cpp-assignment-template/hw01/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
