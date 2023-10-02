14fd759099bbd7e08a80d8b65998331625b4f832
1chess <1chessMic@gmail.com>
2023-10-03 00:08:01 +0400

    hw03 changed

--- hw03/src/include/hw03.h
+++ hw03/src/include/hw03.h
@@ -4,4 +4,4 @@
 
 void hw03(char* str, const char* pattern);
 int _get_start_index(char* str, const char* pattern);
-bool _is_equals(char* str, const char* pattern, int32_t i);
+bool _is_equals(char* str, const char* pattern, size_t i);
