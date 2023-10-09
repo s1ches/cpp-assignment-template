c8263613788767520ab6e3d5c25da30a8206a40b
1chess <1chessMic@gmail.com>
2023-10-10 00:51:21 +0400

    hw04_6

--- hw04/src/hw04.cpp
+++ hw04/src/hw04.cpp
@@ -11,10 +11,8 @@ std::size_t hw04(const int* input_array, std::size_t elem_count)
     std::size_t out_data_size = 1;
     std::size_t* out_data = new std::size_t[out_data_size]{0};
     
-    if(out_data == nullptr){
-        delete[] input_array;
+    if(out_data == nullptr)
         throw std::runtime_error{"Memory was not allocated"};
-    }
 
     for(std::size_t i = 0;i < elem_count && input_array[i] >= 0; i++) {
         out_data[out_data_size-1] = input_array[i];
@@ -22,10 +20,8 @@ std::size_t hw04(const int* input_array, std::size_t elem_count)
         if(i+1 != elem_count && input_array[i+1] >= 0)
             out_data = realloc(out_data, ++out_data_size);
         
-        if(out_data == nullptr){
-            delete[] input_array;
+        if(out_data == nullptr)
             throw std::runtime_error{"Memory was not allocated"};
-        }
     }
 
     std::size_t sum = 0;
